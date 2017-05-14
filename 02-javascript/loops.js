f6c3eb2e63ae2d5f8a35ee272bd19791715e73e4
CristhianMotoche <cristhian.motoche@gmail.com>
2017-05-13 22:47:46 -0500

    Update loops guide

--- 02-javascript/loops.js
+++ 02-javascript/loops.js
@@ -1,14 +1,17 @@
+console.log("For loop:");
 // For loop
 for (var i = 0; i < 20; ++i) {
   console.log("For:", i);
 }
 
+console.log("Foreach loop:");
 // For each
 var array = [1,2,3,4,5];
 for (var item in array) {
   console.log("For each:", item);
 }
 
+console.log("While loop:");
 // While
 var i = 0;
 while (i < 20) {
@@ -16,6 +19,7 @@ while (i < 20) {
   i += 1;
 }
 
+console.log("Switch sentense:");
 // Switch
 var num = null;
 switch (num) {
