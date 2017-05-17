7b4bd865fbf44cdf0eb9950ccc77df71df495b49
CristhianMotoche <cristhian.motoche@gmail.com>
2017-05-17 14:05:13 -0500

    Example JS

--- 02-javascript/app.js
+++ 02-javascript/app.js
@@ -4,11 +4,21 @@
  * This file is for testing things
  */
 
-function holaMundo(){
-  console.log('Hello world!');
-}
+var Cristhian = {
+  nombre: "Cristhian",
+  apellido: "Motoche",
+  printName: function () {
+    console.log("Nombre:", Cristhian.nombre);
+    console.log("Apellido:", Cristhian.apellido);
+  },
+  year: function () {
+    return (new Date()).getFullYear();
+  },
+  compareAges: function (age) {
+    console.log(Math.abs(age - this.year()));
+  }
+};
 
-function myfunc(num) {
-  return num*2;
-}
-console.log(myfunc()*2);
+Cristhian.printName();
+Cristhian.year();
+Cristhian.compareAges(20);
