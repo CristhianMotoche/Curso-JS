5e6f7719718ba6a4e883a88562370f9410e28a8e
CristhianMotoche <cristhian.motoche@gmail.com>
2017-05-17 14:48:06 -0500

    Update component

--- 04-angular/App/src/app/app.component.ts
+++ 04-angular/App/src/app/app.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'raiz',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Hello world!';
   name = 'Cristhian';
   lastname = 'Motoche';
+
+  constructor(){
+    console.log("Hello!");
+    this.holaMundo();
+    console.log(this.greet("Cristhian"));
+  }
+
+  holaMundo(){
+    console.log("Hello world!");
+  }
+
+  greet(name:string, lastname?: string): string {
+    return `Hello ${name} ${lastname}`;
+  }
+}
+
+interface UserInterface {
+  name: string,
+  lastname: string
 }
