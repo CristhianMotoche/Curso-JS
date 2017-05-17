97744a1d6d4dcd0945c5efcc517ecdf53465678d
CristhianMotoche <cristhian.motoche@gmail.com>
2017-05-17 13:29:30 -0500

    First script typescript

--- 03-typescript/helloworld.ts
+++ 03-typescript/helloworld.ts
@@ -1,1 +1,29 @@
-console.log("Hello world!");
+interface UsuarioLogin {
+  nombre: string,
+  apellido: string,
+  direccion?: string
+}
+
+let usuario:UsuarioLogin = {
+  nombre: "LoL",
+  apellido: "xD",
+  direccion: "asdf"
+}
+
+class Persona {
+  name:string = "Nom";
+  private apellido: string;
+  public fechaNac:Date;
+  constructor(name, apellido, fechaNac) {
+    this.name = name;
+    this.apellido = apellido;
+    this.fechaNac = fechaNac;
+  }
+
+  imprimir():UsuarioLogin{
+    return { nombre: this.name, apellido: this.apellido };
+  }
+}
+
+let cm = new Persona("Cristhian", "Motoche", new Date());
+console.log(cm.imprimir());
