ad957da77c06c0a25e358faee77e692eae9a5dde
CristhianMotoche <cristhian.motoche@gmail.com>
2017-06-11 14:34:33 -0500

    Delete and Add users

--- 04-angular/Users/src/app/app.component.ts
+++ 04-angular/Users/src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Http } from '@angular/http';
+
+import { User } from "app/Classes/User";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  constructor(private _http: Http) { }
+
+  newUser:User = new User();
+  users?:User[]
+
+  createUser(){
+    this._http
+      .post("http://localhost:8000/usuario", this.newUser)
+      .subscribe(
+        (response) => {
+          let newUser:User = response.json();
+          this.users.push(newUser);
+        },
+        (error) => { console.log(error); },
+        () => {}
+      )
+  }
+
+  loadUsers(){
+    this._http
+      .get("http://localhost:8000/usuario")
+      .subscribe(
+        (response) => {
+          this.users = response.json();
+        },
+        (error) => { console.log("[ERROR]:", error) },
+        () => {}
+      )
+  }
+
+  deleteUser(id:string){
+    this._http
+      .delete(`http://localhost:8000/usuario/${id}`)
+      .subscribe(
+        (response) => {
+          console.log(response);
+          this.users = this.users.filter((user) => user.id !== id);
+        },
+        (error) => { console.log("[ERROR]:", error) },
+        () => {}
+      )
+  }
+
+  ngOnInit() {
+  }
 }
